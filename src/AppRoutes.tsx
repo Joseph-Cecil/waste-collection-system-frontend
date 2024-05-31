76876270b50d7d306a93d4241589aa5f666529c3
Joseph Cecil Odoi <88081064+Joseph-Cecil@users.noreply.github.com>
2024-05-31 19:11:13 +0000

    Lastest Updates

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ContactUsPage from "./pages/ContactUsPage";
-import LearnPage from "./pages/AboutUs.tsx";
-import AcademyPage from "./pages/OurVision.tsx";
 import AuthenticationPage from "./pages/AuthenticationPage.tsx";
 import Layout from "./Layout/Layout.tsx";
 import ClientDashboard from "./pages/ClientDashboard.tsx";
@@ -11,6 +9,8 @@ import SpecialRequest from "./components/SpecialRequest.tsx";
 import SpecialTakeoutStatus from "./components/SpecialTakeoutStatus.tsx";
 import {TakeOutsTable} from "./components/ClientDashboard/TakeOutsTable.tsx";
 import { CancelRequest } from "./components/ClientDashboard/CancelRequest.tsx";
+import AboutUs from "./pages/AboutUs.tsx";
+import OurVision from "./pages/OurVision.tsx";
 
 function App() {
   return (
@@ -19,8 +19,8 @@ function App() {
         <Route path="/" element={<Layout/>}>
         <Route index element={<HomePage />} />
         <Route path="/contact" element={<ContactUsPage />} />
-        <Route path="/learn" element={<LearnPage />} />
-        <Route path="/academy" element={<AcademyPage />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/vision" element={<OurVision />} />
         <Route path="/auth" element={<AuthenticationPage />} />
         <Route path="/auth/register" element={<AuthenticationPage />} />
         <Route path="/auth/client-dashboard" element={<ClientDashboard/>} />
--- src/components/HeaderMegaMenu.tsx
+++ src/components/HeaderMegaMenu.tsx
@@ -2,137 +2,78 @@ import {
   HoverCard,
   Group,
   Button,
-  UnstyledButton,
-  Text,
-  SimpleGrid,
-  ThemeIcon,
-  Anchor,
   Divider,
   Center,
   Box,
   Burger,
   Drawer,
-  Collapse,
   ScrollArea,
   rem,
-  useMantineTheme,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import {
-  IconNotification,
-  IconCode,
-  IconBook,
-  IconChartPie3,
-  IconFingerprint,
-  IconCoin,
-  IconChevronDown,
-} from "@tabler/icons-react";
+
 import { Link } from "react-router-dom";
 import classes from "./HeaderMegaMenu.module.css";
 import LightAndDarkTheme from "./LightAndDarkTheme";
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: "Open source",
-    description: "This Pokémon’s cry is very loud and distracting",
-  },
-  {
-    icon: IconCoin,
-    title: "Free for everyone",
-    description: "The fluid of Smeargle’s tail secretions changes",
-  },
-  {
-    icon: IconBook,
-    title: "Documentation",
-    description: "Yanma is capable of seeing 360 degrees without",
-  },
-  {
-    icon: IconFingerprint,
-    title: "Security",
-    description: "The shell’s rounded shape and the grooves on its.",
-  },
-  {
-    icon: IconChartPie3,
-    title: "Analytics",
-    description: "This Pokémon uses its flying ability to quickly chase",
-  },
-  {
-    icon: IconNotification,
-    title: "Notifications",
-    description: "Combusken battles with the intensely hot flames it spews",
-  },
-];
+
 
 export function HeaderMegaMenu() {
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-  const theme = useMantineTheme();
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
 
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon style={{ width: rem(22), height: rem(22) }} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
+  
 
   return (
-    <Box style={{ height: '100%', display: 'flex', flexDirection: 'column' }} mb={0}>
+    <Box
+      style={{ height: "100%", display: "flex", flexDirection: "column" }}
+      mb={0}
+    >
       <header className={classes.header}>
         <Group justify="space-between" h="100%">
           <LightAndDarkTheme />
           <Group h="100%" gap={0} visibleFrom="sm">
-            <a href="/" className={classes.link}>Home</a>
-            <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-              
-                <Link to="/contact" className={classes.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>Contact</Box>
-                  </Center>
-                </Link>
-
-              <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
-                <Group justify="space-between" px="md">
-                  <Text fw={500}>Features</Text>
-                  <Anchor href="#" fz="xs">View all</Anchor>
-                </Group>
-                <Divider my="sm" />
-                <SimpleGrid cols={2} spacing={0}>
-                  {links}
-                </SimpleGrid>
-                <div className={classes.dropdownFooter}>
-                  <Group justify="space-between">
-                    <div>
-                      <Text fw={500} fz="sm">Get started</Text>
-                      <Text size="xs" c="dimmed">Their food sources have decreased, and their numbers</Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
+            <a href="/" className={classes.link}>
+              Home
+            </a>
+            <HoverCard
+              width={600}
+              position="bottom"
+              radius="md"
+              shadow="md"
+              withinPortal
+            >
+              <Link to="/contact" className={classes.link}>
+                <Center inline>
+                  <Box component="span" mr={5}>
+                    Contact
+                  </Box>
+                </Center>
+              </Link>
             </HoverCard>
-            <a href="#" className={classes.link}>Learn</a>
-            <a href="#" className={classes.link}>Academy</a>
+            <a href="/about" className={classes.link}>
+              About Us
+            </a>
+            <a href="/vision" className={classes.link}>
+              Our Vision
+            </a>
           </Group>
           <Group visibleFrom="sm">
-            <Link to="/auth"><Button variant="default">Log in</Button></Link>
-            <Link to="/auth/register"><Button>Sign up</Button></Link>
+            <Link to="/auth">
+              <Button variant="default">Log in</Button>
+            </Link>
+            <Link to="/auth/register">
+              <Button>Sign up</Button>
+            </Link>
           </Group>
-          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            hiddenFrom="sm"
+          />
         </Group>
       </header>
-      <Box style={{ flex: '1 0 auto', overflow: 'auto' }}>
+      <Box style={{ flex: "1 0 auto", overflow: "auto" }}>
         <Drawer
           opened={drawerOpened}
           onClose={closeDrawer}
@@ -144,20 +85,21 @@ export function HeaderMegaMenu() {
         >
           <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
             <Divider my="sm" />
-            <a href="#" className={classes.link}>Home</a>
-            <UnstyledButton className={classes.link} onClick={toggleLinks}>
-              <Center inline>
-                <Box component="span" mr={5}>Features</Box>
-                <IconChevronDown style={{ width: rem(16), height: rem(16) }} color={theme.colors.blue[6]} />
-              </Center>
-            </UnstyledButton>
-            <Collapse in={linksOpened}>{links}</Collapse>
-            <a href="#" className={classes.link}>Learn</a>
-            <a href="#" className={classes.link}>Academy</a>
+            <a href="/" className={classes.link}>
+              Home
+            </a>
+            <a href="/contact" className={classes.link}>
+              Contact Us
+            </a>
+
             <Divider my="sm" />
             <Group justify="center" pb="xl" px="md">
-              <Link to="/auth"><Button variant="default">Log in</Button></Link>
-              <Link to="/auth/register"><Button>Sign up</Button></Link>
+              <Link to="/auth">
+                <Button variant="default">Log in</Button>
+              </Link>
+              <Link to="/auth/register">
+                <Button>Sign up</Button>
+              </Link>
             </Group>
           </ScrollArea>
         </Drawer>
